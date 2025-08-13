87d1c36459e8025c4c14d9d01c208e3712f66270
kelven-jing <3910387331@qq.com>
2025-08-13 13:06:45 +0800

    Update diagnose.js

--- pages/api/diagnose.js
+++ pages/api/diagnose.js
@@ -2,11 +2,11 @@ const formidable = require("formidable");
 const fs = require("fs");
 const FormData = require("form-data");
 
-export const config = {
+module.exports.config = {
   api: { bodyParser: false },
 };
 
-export default async function handler(req, res) {
+module.exports.default = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -45,4 +45,4 @@ export default async function handler(req, res) {
       });
     }
   });
-}
+};
